d705fd2ba2093576766c4d166c3f5fa1ee766436
Gerlan de Paula <jgerlan@gmail.com>
2019-12-18 11:51:19 -0300

    [#0001] Prepara estrutura do projeto

---
+++ API.BookStore/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace API.BookStore.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ API.BookStore/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace API.BookStore.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
